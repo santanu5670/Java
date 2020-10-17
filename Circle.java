68d6fb82388dbf7c5e2fa8d557b291e16af258fd
Santanu Saha <santanu2539@gmail.com>
2020-10-17 19:35:28 +0530

    Find The Area Of a Circle

---
+++ Circle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Circle{
+	 public static void main(String[] args) {
+		double r,a;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Radious of Circle");
+		r=sc.nextInt();
+		a=3.14*r*r;
+		System.out.println("The Area Of a Circle is="+a);
+    }
+}
